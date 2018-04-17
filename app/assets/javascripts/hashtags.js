29760b3bc6ff90b45d96e784b26252da90873b42
Matthew Hadley <matthew.g.hadley@gmail.com>
2018-04-16 21:56:01 -0600

    adds copy to clipboard button

--- app/assets/javascripts/hashtags.js
+++ app/assets/javascripts/hashtags.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function copyBlock() {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($('.hashtag-block').text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+  copySuccess();
+}
+
+function copySuccess() {
+  $('.success-message').show()
+  $('.success-message').fadeOut(2000)
+}
+
+$(document).on('turbolinks:load', function() {
+  $('#copy-btn').click(copyBlock);
+})
