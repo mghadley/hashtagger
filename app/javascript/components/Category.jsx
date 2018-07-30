6cfb060a7654070b2f1b2f50090459706dee2a86
Matthew Hadley <matthew.g.hadley@gmail.com>
2018-07-29 20:43:08 -0600

    adds margin between categories

--- app/javascript/components/Category.jsx
+++ app/javascript/components/Category.jsx
@@ -4,7 +4,7 @@ import UserHashCats from "./UserHashCats"
 class Category extends React.Component {
   render() {
     return(
-      <div>
+      <div className='category-block'>
         <h3>{this.props.category.name}</h3>
         <UserHashCats category={this.props.category} csrf_token={this.props.csrf_token} />
       </div>
